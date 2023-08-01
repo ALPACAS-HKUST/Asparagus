f4c4fce59763c7f071df808c19a5fd2ce9ad778a
Soroush Farrokhnia <destrat18@gmail.com>
2023-08-01 15:07:15 +0800

    First commit

---
+++ illustration_examples/run_voting.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+file_name=voting
+contract_name=Voting
+function_name="winningProposal"
+
+cd src/
+python3 synthesizer.py \
+    --root_dir ../illustration_examples/${file_name} \
+    --file_name ${file_name}.sol \
+    --contract_name ${contract_name} \
+    --function_name ${function_name} \
+    --output_file ${file_name}_${contract_name}_${function_name}.out \
+    --use_const_cnt 1 --print_verbose
+
+rm *.smt2 *.model *.gas
+
+cd -
